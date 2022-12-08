514310d4152ac44b9b602d74397868d0b7495963
Luis <luisriveros26@gmail.com>
2022-12-08 14:16:48 -0700

    updated contact section

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,95 +1,116 @@
-import React, { useState } from "react";
-import { validateEmail } from '../../utils/helper';
+import React from "react";
+// import { validateEmail } from '../../utils/helper';
 
-function Contact() {
-    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
-    const { name, email, message } = formState;
-    const [errorMessage, setErrorMessage] = useState('');
+// function Contact() {
+//     const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+//     const { name, email, message } = formState;
+//     const [errorMessage, setErrorMessage] = useState('');
 
-    function handleChange(event) {
-        if (event.target.name === 'email') {
-            const isValid = validateEmail(event.target.value);
-            console.log(isValid);
+//     function handleChange(event) {
+//         if (event.target.name === 'email') {
+//             const isValid = validateEmail(event.target.value);
+//             console.log(isValid);
 
-            if (!isValid) {
-                setErrorMessage('Your email is invalid.');
-            } else {
-                setErrorMessage('');
-            }
-        } else {
-            if (!event.target.value.length) {
-                setErrorMessage(`${event.target.name} is required.`)
-            } else {
-                setErrorMessage('');
-            }
-        }
-        console.log('errorMessage', errorMessage);
-        if (!errorMessage) {
-            setFormState({ ...formState, [event.target.name]: event.target.value })
-        }
-    }
+//             if (!isValid) {
+//                 setErrorMessage('Your email is invalid.');
+//             } else {
+//                 setErrorMessage('');
+//             }
+//         } else {
+//             if (!event.target.value.length) {
+//                 setErrorMessage(`${event.target.name} is required.`)
+//             } else {
+//                 setErrorMessage('');
+//             }
+//         }
+//         console.log('errorMessage', errorMessage);
+//         if (!errorMessage) {
+//             setFormState({ ...formState, [event.target.name]: event.target.value })
+//         }
+//     }
 
-    function handleFormClear(e) {
-        setFormState({...formState, [e.target.name]: e.target.value});
-    }
+//     function handleFormClear(e) {
+//         setFormState({...formState, [e.target.name]: e.target.value});
+//     }
 
-    function handleSubmit(event) {
-        event.preventDefault();
-        setFormState({name: '', email: '', message: ''})
-        console.log(formState);
-    }
+//     function handleSubmit(event) {
+//         event.preventDefault();
+//         setFormState({name: '', email: '', message: ''})
+//         console.log(formState);
+//     }
+//     return (
+//         <section>
+            // <div className="row justify-content-center mt-3 m-0">
+            //     <div className="shadow-lg card col-sm-6 p-0">
+            //         <h3 className="card-header p-0 text-center" style={{backgroundColor: "#cad2c5", color: "#264653" }}>Contact Me</h3>
+            //         <form id="contact-form" onSubmit={handleSubmit}>
+            //             <div className="card-body rounded" style={{backgroundColor: "#ffffff"}}>
+            //                 <div className="mb-3">
+            //                     <label htmlFor="name" className="form-label text-start" style={{color: "#264653"}}>Name:</label>
+            //                     <input 
+            //                     type="text" 
+            //                     className="form-control" 
+            //                     value={name}
+            //                     id="exampleFormControlInput1" 
+            //                     placeholder="John Doe" 
+            //                     onBlur={handleChange} 
+            //                     name="name"
+            //                     onChange={handleFormClear}
+            //                     ></input>
+            //                 </div>
+            //                 <div className="mb-3">
+            //                     <label htmlFor="email" className="form-label text-start" style={{color: "#264653"}}>Email address:</label>
+            //                     <input 
+            //                     type="email" 
+            //                     className="form-control" 
+            //                     value={email}
+            //                     id="exampleFormControlInput1" 
+            //                     placeholder="name@example.com" 
+            //                     onBlur={handleChange} 
+            //                     name="email"
+            //                     onChange={handleFormClear}
+            //                     ></input>
+            //                 </div>
+            //                 <div className="mb-3">
+            //                     <label htmlFor="message" className="form-label" style={{color: "#264653"}}>Message:</label>
+            //                     <textarea 
+            //                     className="form-control" 
+            //                     value={message}
+            //                     id="exampleFormControlTextarea1" 
+            //                     rows="3" 
+            //                     onBlur={handleChange} 
+            //                     name="message"
+            //                     onChange={handleFormClear}
+            //                     ></textarea>
+            //                 </div>
+            //                 {errorMessage && (
+            //                     <div>
+            //                         <p className="error-text">{errorMessage}</p>
+            //                     </div>
+            //                 )}
+
+            //                 <button className="btn" type="submit" style={{backgroundColor: "#264653", color: "#ffffff"}}>Submit</button>
+            //             </div>
+            //         </form>
+            //     </div>
+            // </div>
+//         </section>
+//     )
+// }
+
+function Contact () {
     return (
         <section>
             <div className="row justify-content-center mt-3 m-0">
                 <div className="shadow-lg card col-sm-6 p-0">
                     <h3 className="card-header p-0 text-center" style={{backgroundColor: "#cad2c5", color: "#264653" }}>Contact Me</h3>
-                    <form id="contact-form" onSubmit={handleSubmit}>
-                        <div className="card-body rounded" style={{backgroundColor: "#ffffff"}}>
-                            <div className="mb-3">
-                                <label htmlFor="name" className="form-label text-start" style={{color: "#264653"}}>Name:</label>
-                                <input 
-                                type="text" 
-                                className="form-control" 
-                                value={name}
-                                id="exampleFormControlInput1" 
-                                placeholder="John Doe" 
-                                onBlur={handleChange} 
-                                name="name"
-                                onChange={handleFormClear}
-                                ></input>
-                            </div>
-                            <div className="mb-3">
-                                <label htmlFor="email" className="form-label text-start" style={{color: "#264653"}}>Email address:</label>
-                                <input 
-                                type="email" 
-                                className="form-control" 
-                                value={email}
-                                id="exampleFormControlInput1" 
-                                placeholder="name@example.com" 
-                                onBlur={handleChange} 
-                                name="email"
-                                onChange={handleFormClear}
-                                ></input>
-                            </div>
-                            <div className="mb-3">
-                                <label htmlFor="message" className="form-label" style={{color: "#264653"}}>Message:</label>
-                                <textarea 
-                                className="form-control" 
-                                value={message}
-                                id="exampleFormControlTextarea1" 
-                                rows="3" 
-                                onBlur={handleChange} 
-                                name="message"
-                                onChange={handleFormClear}
-                                ></textarea>
-                            </div>
-                            {errorMessage && (
-                                <div>
-                                    <p className="error-text">{errorMessage}</p>
-                                </div>
-                            )}
-
-                            <button className="btn" type="submit" style={{backgroundColor: "#264653", color: "#ffffff"}}>Submit</button>
+                    <form id="contact-form">
+                        <div className="card-body rounded text-center" style={{backgroundColor: "#ffffff"}}>
+                            <p>
+                                I am currently looking for opportunities to be a full stack developer. If you have any questions please feel free to email me at luisriveros26@gmail.com or click the button below. Also feel free 
+                                to look at my linkedin page using the thumbnail below. Thank you for taking the time to look at my portfolio.
+                            </p>
+                            <a className="btn" type="submit" href='mailto:luisriveros26@gmail.com' style={{backgroundColor: "#264653", color: "#ffffff"}}>Email Me</a>
                         </div>
                     </form>
                 </div>
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -8,7 +8,7 @@ function Project(props) {
         {
             id: '0',
             name: 'World Wide Jerky',
-            description: 'Group project for an E-commerce web page for a local beef jerky company.',
+            description: 'Group project for an E-commerce web page for a local beef jerky company. Project used MERN stack, Apollo, GraphQL, JWT, and Bcrypt.',
             deployedLink: 'https://worldwidejerky.herokuapp.com/',
             github: 'https://github.com/mjzabriskie/world-wide-jerky',
             projectImage: 'WorldWideJerky'
@@ -16,7 +16,7 @@ function Project(props) {
         {
             id: '1',
             name: 'Weather Dashboard',
-            description: 'App using weather API that will give the user the weather based off of a city name. Also shows the 5-day forecast for the city.',
+            description: 'App using weather API that will give the user the weather based off of a city name. Also shows the 5-day forecast for the city. App was built using Javascript, HTML, CSS, and Server-Side API. ',
             deployedLink: 'https://luimr88.github.io/weather-dashboard/',
             github: 'https://github.com/luimr88/weather-dashboard',
             projectImage: 'WeatherDash'
@@ -24,7 +24,7 @@ function Project(props) {
         {
             id: '2',
             name: 'Marvelous Gifs',
-            description: 'Group project using Marvel Comics API where the user can get the bio, comics, and gifs to a Marvel character they search.',
+            description: 'Group project using Marvel Comics API where the user can get the bio, comics, and gifs to a Marvel character they search. Project used HTML, CSS, Bulma, Javascript, Marvel API, Giphy API.',
             deployedLink: 'https://mjzabriskie.github.io/marvel-ous-gifs/',
             github: 'https://github.com/mjzabriskie/marvel-ous-gifs',
             projectImage: 'MarvelousGifs'
@@ -32,7 +32,7 @@ function Project(props) {
         {
             id: '3',
             name: "A Hero's Outline",
-            description: "Group project where a user can create an outline for writing a story using Dan Harmon's story circle concept, while being able to save story outlines to their account.",
+            description: "Group project where a user can create an outline for writing a story using Dan Harmon's story circle concept, while being able to save story outlines to their account. Project used Express, MYSQL, MYSQL2, Sequelize, Handlebars, HTML, CSS, and Javascript.",
             deployedLink: 'https://herooutline.herokuapp.com/',
             github: 'https://github.com/mjzabriskie/a-heros-outline',
             projectImage: 'HerosOutline'
@@ -40,7 +40,7 @@ function Project(props) {
         {
             id: '4',
             name: 'Password Generator',
-            description: 'Web app where a user can generate a random password of different lengths, symbols, letters, and numbers.',
+            description: 'Web app where a user can generate a random password of different lengths, symbols, letters, and numbers. App was built using Javascript, HTML, CSS.',
             deployedLink: 'https://luimr88.github.io/password-generator/',
             github: 'https://github.com/luimr88/password-generator',
             projectImage: 'PasswordGen'
@@ -48,7 +48,7 @@ function Project(props) {
         {
             id: '5',
             name: 'PWA Budget Tracker',
-            description: 'Budget tracking app that can keep track of expenses along with a visual graph. App can be used offline.',
+            description: 'Budget tracking app that can keep track of expenses along with a visual graph. App can be used offline. App was built using PWA, Express, Mongoose, Compression, and Morgan.',
             deployedLink: 'https://stark-woodland-05347.herokuapp.com/',
             github: 'https://github.com/luimr88/pwa-budget-tracker',
             projectImage: 'BudgetTracker'
@@ -56,7 +56,7 @@ function Project(props) {
         {
             id: '6',
             name: 'Note Taker',
-            description: 'Web app where the user is able to to enter notes, save, and delete notes on the webpage.',
+            description: 'Web app where the user is able to to enter notes, save, and delete notes on the webpage. App was built using Express, Javascript, and HTML.',
             deployedLink: 'https://evening-journey-94806.herokuapp.com/',
             github: 'https://github.com/luimr88/note-taker',
             projectImage: 'NoteTaker'
